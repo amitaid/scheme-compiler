340cce68067bff850f632b1f84ee8b61402f6dbd
haskellerator <talzelig@gmail.com>
2013-11-23 15:53:32 +0200

    added predicates for IfThen and IfThenElse and re arranged the classes in tag parser

--- tag_parser.py
+++ tag_parser.py
@@ -1,3 +1,4 @@
+from email._header_value_parser import get_token
 from sexprs import *
 
 
@@ -9,11 +10,27 @@ class AbstractSchemeExpr:
         result, remaining = AbstractSexpr.readFromString(input)
         # dealing with core forms
 
+    # this function gets a token of string and a pair, and extracts the token if available
+    def get_token(token,pair):  # token is a string/char
+        if isinstance(pair,Pair):
+            if str(pair.get_car().get_value()) == token: #  isinstance(pair.get_car(),Symbol) and
+                #print(pair)
+                return (pair.get_car(),pair.get_cdr())
+            else:
+                #print(pair.cdr)
+                pair2 = Pair.get_cdr(pair)
+                return AbstractSchemeExpr.get_token(token,pair2)
+
+        if not isinstance(pair,Nil) and str(pair.get_car().get_value()) == token:
+            return (pair.get_car(),Nil)
+        return None
 
 ### Constant ###
 
 class Constant(AbstractSchemeExpr):
-    pass
+
+    def __init__(self,sexpr):
+        self.expr = sexpr
 
     def is_quated(sexpr): # TODO implement later
         return False
@@ -27,19 +44,19 @@ class Constant(AbstractSchemeExpr):
         else: #TODO add unquated support
             return False
 
-
 ### Variable ###
 
 class Variable(AbstractSchemeExpr):
-    pass
+    def __init__(self,sexpr):
+        self.value = AbstractSexpr.get_value(sexpr)
 
     def is_variable(sexpr):
     # predicate for recognizing if then else expr
-        if isinstance(sexpr,Symbol) and not sexpr in key_words:
+        if isinstance(sexpr,Symbol): #and not Symbol.get_value(sexpr) in key_words:
             return True;
-        elif sexpr in key_words:
+        elif Symbol.get_value(sexpr) in key_words:
             print("Error: is a key word" +  str(sexpr)) #TODO maybe throw exception
-        return False;
+        return True;
 
 
 ### Lambda Forms ###
@@ -63,15 +80,34 @@ class IfThenElse(AbstractSchemeExpr):
     pass
 
     def is_if_then_else(expr):
-        # predicate for recognizing if then else expr
-        pass
+        # first check if it is a pair at all
+        if isinstance(expr,Pair):
+            if_token = expr.get_car()
+            rest_expr = expr.get_cdr()
+            # second check if first token is if and rest is pair
+            if isinstance(if_token,Symbol) and \
+                str(if_token.get_value()) == 'IF' and \
+                isinstance(rest_expr,Pair):
 
-    def is_if_then(expr):
-        # predicate for recognizing ifthen expr
-        pass
+            #    predicate = rest_expr.get_car()  # extracts the pred - not needed
+                rest_expr2 = rest_expr.get_cdr()
 
-    def it2ite(expr): # if then -> if then else transformation
-        pass          # syntactic sugar
+                # third check the then argument exists
+                if isinstance(rest_expr2,Pair):
+
+                 #   then_expr = rest_expr2.get_car() # extracts the then - not needed
+                    rest_expr3 = rest_expr2.get_cdr()
+                    # fourth check if the else arg exists
+
+                    if isinstance(rest_expr3,Pair): # case proper list
+
+                 #       else_expr = rest_expr3.get_car() # extracts the else - not needed
+                        rest = rest_expr3.get_cdr()
+                        if isinstance(rest,Nil):  # checks if the list end is Nil
+                            return True
+        #            elif not isinstance(rest_expr3,Nil): # case not improper
+        #                return True
+        return False
 
 class Applic(AbstractSchemeExpr):
     pass
@@ -87,10 +123,68 @@ class Or(AbstractSchemeExpr):
         # a predicate for recognizing or expressions
         pass
 
-
 class Def(AbstractSchemeExpr):
     pass
 
     def is_def(expr):
         # a predicate for recognizing definition expressions
         pass
+
+
+class syntacticSugar(AbstractSchemeExpr):
+    pass
+
+    # predicate for recognizing ifthen expr
+    def is_if_then(expr):
+        # first check if it is a pair at all
+        if isinstance(expr,Pair):
+            if_token = expr.get_car()
+            rest_expr = expr.get_cdr()
+            # second check if first token is if and rest is pair
+            if isinstance(if_token,Symbol) and \
+                str(if_token.get_value()) == 'IF' and \
+                isinstance(rest_expr,Pair):
+
+            #    predicate = rest_expr.get_car()  # extracts the pred - not needed
+                rest_expr2 = rest_expr.get_cdr()
+
+                # third check the then argument exists
+                if isinstance(rest_expr2,Pair):
+
+                 #   then_expr = rest_expr2.get_car() # extracts the then - not needed
+                    rest = rest_expr2.get_cdr()
+                    # fourth check if the rest is nil
+                    if isinstance(rest,Nil):  # checks if the list end is Nil
+                            return True
+        #            elif not isinstance(rest_expr3,Nil): # case not improper
+        #                return True
+        return False
+        pass
+
+    def is_quasiquated(expr):
+        pass
+
+    def is_let(expr):
+        pass
+
+    def is_letStar(expr):
+        pass
+
+    def is_letrec(expr):
+        pass
+
+    def is_cond(expr):
+        pass
+
+    def is_and(expr):
+        pass
+
+    def it2ite(expr): # if then -> if then else transformation
+        if_token = expr.get_car()
+        rest = expr.get_cdr()
+        pred_expr = rest.get_car()
+        rest2 = rest.get_cdr()
+        rest2.
+    def ic_2_nextedifs(expr): # cond -> if transformation
+        pass
+
--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ import reader
 
 # Zelig, this is the message
 # Amitai, this is a message
+import tag_parser
+
 
 def parse(input):
     print(input + ' => ' + str(sexprs.AbstractSexpr.readFromString(input)[0]))
@@ -12,7 +14,7 @@ def main():
     #print(sexprs.AbstractSexpr.readFromString(
     #    r'(-0H432 +0123/0x52 ab53$$ "bla bla \n \l bla" #\pAge #\tAb #\xFDFA #\M () #(1 2 3 #(1 2 3)) 72 . 5)')[0])
 
-    print(reader.ignorable.match('#;(a b c)'))
+    #print(reader.ignorable.match('#;(a b c)'))
 
     #parse(r';     \n(a)')
 
@@ -36,8 +38,28 @@ def main():
     #print(AbstractSexpr.readFromString('#\\x30')[0])
     #print(AbstractSexpr.readFromString('#\\☺')[0])
 
-
-
+#    parse('(if 5 > 0 then 1 else 2)')
+    x,y = sexprs.AbstractSexpr.readFromString('(if #t 1)')
+    print(x)
+    print(tag_parser.IfThenElse.is_if_then_else(x))
+    print(tag_parser.syntacticSugar.is_if_then(x))
+  #  print(sexprs.Pair.get_car(x))
+  #  print(sexprs.Pair.get_cdr(x).get_car())
+  #  x2,y2 = tag_parser.AbstractSchemeExpr.get_token('IF',x)
+  #  print(x2)
+  #  print(y2)
+  #  x3,y3 = tag_parser.AbstractSchemeExpr.get_token('THEN',x)
+  #  print(x3)
+  #  print(y3)
+  #  x4,y4 = tag_parser.AbstractSchemeExpr.get_token('ELSE',x)
+  #  print(x4)
+  #  print(y4)
+  #  x5,y5 = tag_parser.AbstractSchemeExpr.get_token('1',x)
+  #  print(x5)
+  #  print(y5)
+  #  x6,y6 = tag_parser.AbstractSchemeExpr.get_token('2',x)
+  #  print(x6)
+  #  print(y6)
 if __name__ == '__main__':
     main()
 
