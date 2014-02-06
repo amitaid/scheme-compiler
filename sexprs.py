e33770062287ff5dcaa23a56a948e948f15d8377
amitaid <amitai.d@gmail.com>
2014-02-06 18:45:11 +0200

    Removed symbol_list from sexprs. Will be done in tag_parser now.

--- sexprs.py
+++ sexprs.py
@@ -1,7 +1,5 @@
 from reader import *
 
-symbol_list = []
-
 
 class AbstractSexpr:
     def get_value(self):
@@ -152,7 +150,6 @@ class Symbol(AbstractSexpr):
     def __init__(self, symbol_str):
         self.value = symbol_str
         self.length = len(symbol_str)
-        symbol_list.append(self)
 
     def __str__(self):
         return self.value
