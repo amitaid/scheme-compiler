f9e24890a5d77322b020faba0bf1d3ca32b6503e
amitaid <amitai.d@gmail.com>
2014-02-09 14:15:37 +0200

    Fraction printing. Needs work.

--- arch/lib/scheme/write_sob_fraction.asm
+++ arch/lib/scheme/write_sob_fraction.asm
@@ -10,18 +10,19 @@
   PUSH(FP);
   PUSH(R1);
   MOV(FP, SP);
-  MOV(R0, FPARG(0));
-  MOV(R1, INDD(R0, 2));
+  MOV(R0, FPARG(1));
+  MOV(R1, INDD(R0, 1));
   PUSH(INDD(R1, 1));
   CALL(WRITE_INTEGER);
   DROP(1);
   PUSH(IMM('/'));
   CALL(PUTCHAR);
   DROP(1);
-  MOV(R1, INDD(R0, 1));
+  MOV(R1, INDD(R0, 2));
   PUSH(INDD(R1, 1));
   CALL(WRITE_INTEGER);
   DROP(1);
+  POP(R1);
   POP(FP);
   RETURN;
 
