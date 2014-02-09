f076b1ccada0b737c0c90ae8e148c3ee65b3a5d8
amitaid <amitai.d@gmail.com>
2014-02-09 22:09:49 +0200

    Plus fix

--- arch/lib/math/plus.asm
+++ arch/lib/math/plus.asm
@@ -6,9 +6,10 @@
 
  PLUS:
   MOV(R0, IMM(0));
-  MOV(R1, FPARG(0));
+  MOV(R1, FPARG(1));
+  ADD(R1, IMM(2));
  PLUS_LOOP:
-  CMP(R1, IMM(0));
+  CMP(R1, IMM(1));
   JUMP_EQ(PLUS_EXIT);
   ADD(R0, FPARG(R1));
   SUB(R1, IMM(1));
