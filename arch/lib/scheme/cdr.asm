25ba612dfc3d06f54be8fad75fbab60eb1844a4b
amitaid <amitai.d@gmail.com>
2014-02-11 23:03:56 +0200

    Cons, car, cdr

---
+++ arch/lib/scheme/cdr.asm
@@ -0,0 +1,26 @@
+/* cdr.asm
+ * Takes a pair object and returns the first item
+ *
+ * Programmer: Amitai Degani, Tal Zelig, 2014
+ */
+
+ CDR:
+  PUSH(FP);
+  MOV(FP,SP);
+
+  MOV(R0, FPARG(1));
+  CMP(R0, IMM(1));
+
+  // ERROR
+
+  MOV(R0, FPARG(2));
+  CMP(IND(R0), T_PAIR);
+
+  // ERROR
+
+  MOV(R0, INDD(R0,2));
+
+  POP(FP);
+  RETURN;
+
+
