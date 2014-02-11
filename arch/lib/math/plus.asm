c32bd1ae355c8fb94d60db7faa1f93f9239150bc
amitaid <amitai.d@gmail.com>
2014-02-11 07:28:48 +0200

    Plus changes to support fraction. Added make_sob_number file.

--- arch/lib/math/plus.asm
+++ arch/lib/math/plus.asm
@@ -7,20 +7,44 @@
  PLUS:
   PUSH(FP);
   MOV(FP,SP);
-  MOV(R0, IMM(0));
-  MOV(R1, FPARG(1));
-  ADD(R1, IMM(1));
- PLUS_LOOP:
-  MOV(R2, FPARG(R1));
-  ADD(R0, INDD(R2,1));
-  DECR(R1);
-  CMP(R1, IMM(1));
-  JUMP_NE(PLUS_LOOP);
+  MOV(R0, IMM(0)); // Result numerator
+  MOV(R1, IMM(1)); // Result denumerator
+  MOV(R5, FPARG(1)); // Argument number
+  ADD(R5, IMM(2));
 
-  PUSH(R0);
-  CALL(MAKE_SOB_INTEGER);
-  DROP(1);
+ PLUS_LOOP:
+  DECR(R5);
+  CMP(R5,1);
+  JUMP_EQ(PLUS_EXIT);
+  MOV(R2, FPARG(R5));
+  CMP(IND(R2), T_FRACTION);
+  JUMP_EQ(PLUS_FRAC);
+  CMP(IND(R2), T_INTEGER);
+  JUMP_EQ(PLUS_INT);
+
+ PLUS_INT:
+  MOV(R2, INDD(R2,1)); // Number is int, only deal with numerator
+  MUL(R2,R1);
+  ADD(R0,R2);
+  JUMP(PLUS_LOOP);
+
+ PLUS_FRAC:
+  MOV(R2, INDD(R2,1)); // Fraction numerator
+  MOV(R3, INDD(R2,2)); // Fraction denumerator
+  CMP(R1,R3);
+  JUMP_EQ(PLUS_FRAC_SAME_DENUM);
+  MUL(R2,R1);       // Mechane meshutaf
+  MUL(R0,R3);
+  MUL(R1,R3);
+ PLUS_FRAC_SAME_DENUM:
+  ADD(R0,R2);
+  JUMP(PLUS_LOOP);
 
  PLUS_EXIT:
+  PUSH(R1);
+  PUSH(R0);
+  CALL(MAKE_SOB_NUMBER);
+  DROP(2);
+
   POP(FP);
   RETURN;
---
+++ arch/lib/scheme/make_sob_number.asm
@@ -0,0 +1,33 @@
+/* scheme/make_sob_fraction.asm
+ * Takes two integers, numerator and denumerator.
+ * Determines whether they represent an int or fraction,
+ * and returns the corresponding object.
+ *
+ * Programmer: Amitai Degani, 2014
+ */
+
+ MAKE_SOB_NUMBER:
+  PUSH(FP);
+  MOV(FP, SP);
+
+  MOV(R0, FPARG(0));
+  MOV(R1, FPARG(1));
+  REM(R0,R1);
+  JUMP_NE(MAKE_NUMBER_FRAC);
+
+ MAKE_NUMBER_INT:
+  DIV(R0,R1);
+  PUSH(R0);
+  CALL(MAKE_SOB_INTEGER);
+  DROP(1);
+  JUMP(MAKE_NUMBER_EXIT);
+
+ MAKE_NUMBER_FRAC:
+  PUSH(R1);
+  PUSH(R0);
+  CALL(MAKE_SOB_FRACTION);
+  DROP(2);
+
+ MAKE_NUMBER_EXIT:
+  POP(FP);
+  RETURN;
