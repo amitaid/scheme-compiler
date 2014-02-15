d67b9f8453ee0bbc861ff83bd87293c8dfb9020a
amitaid <amitai.d@gmail.com>
2014-02-15 23:55:28 +0200

    Fixed number comparisons with =

--- arch/lib/math/equal.asm
+++ arch/lib/math/equal.asm
@@ -29,22 +29,32 @@
   JUMP_EQ(EQUAL_SUCCESS);
 
   MOV(R2, INDD(R0,1));  // R2, R3 hold the later order number
+  MOV(R2, INDD(R2,1));
   CMP(IND(R0), T_FRACTION);
   JUMP_EQ(EQUAL_FRACTION_PREP_1);
+
   MOV(R3, IMM(1));   // R0 holds an int. Numerator only.
+  MOV(R0, INDD(R0,1));
   JUMP(EQUAL_SECOND_NUMBER);
+
  EQUAL_FRACTION_PREP_1:
   MOV(R3, INDD(R0,2));
+  MOV(R3, INDD(R3,1));
 
  EQUAL_SECOND_NUMBER:
   MOV(R0, FPARG(R1));
   MOV(R4, INDD(R0,1));  // R4, R5 hold the earlier order number
+  MOV(R4, INDD(R4,1));
+
   CMP(IND(R0), T_FRACTION);
   JUMP_EQ(EQUAL_FRACTION_PREP_2);
+
   MOV(R5, IMM(1));   // R0 holds an int. Numerator only.
   JUMP(EQUAL_COMPARE);
+
  EQUAL_FRACTION_PREP_2:
   MOV(R5, INDD(R0,2));
+  MOV(R5, INDD(R5,1));
 
  EQUAL_COMPARE:
   MUL(R2,R5);
